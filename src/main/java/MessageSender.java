f990aef5fcb0837119104f9ac1051b6faa9cdede
Sachin <sachinkmr375@gmail.com>
2020-12-12 22:59:20 +0530

    rabbit mq starting

--- src/main/java/MessageSender.java
+++ src/main/java/MessageSender.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class MessageSender {
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class MessageSender {
+
+    private final static String QUEUE_NAME = "Prod_Queue";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        /*
+         * Here we connect to a broker on the local machine - hence the localhost. If we wanted to
+         * connect to a broker on a different machine we'd simply specify its name or IP address
+         * here.
+         */
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel())
+
+        {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            Product product = new Product();
+            product.setProdId(100);
+            product.setProdName("Laptop");
+
+            byte[] byteArray = getByteArray(product);
+
+            channel.basicPublish("", QUEUE_NAME, null, byteArray);
+            System.out.println(" [x] Sent '" + product + "'");
+        }
+        catch (Exception exe)
+        {
+            exe.printStackTrace();
+        }
+    }
+
+    private static byte[] getByteArray(Product product) throws IOException
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(product);
+        return out.toByteArray();
+    }
+
+
 }
--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private int prodId;
+    private String prodName;
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
 }
